5cffe2113caa5340d1d4d1574692b24fa72f4dd5
orgtarefas <orgtarefas.site@gmail.com>
2026-04-09 15:36:23 -0300

    Update home_psicologo.js

--- home_psicologo.js
+++ home_psicologo.js
@@ -20,7 +20,7 @@ export class HomePsicologo {
             <div class="home-container">
                 <div class="header">
                     <div class="header-logo">
-                        <img src="imagens/logo.png" alt="Vitality" class="header-logo-img">
+                        <img src="imagens/logo.png" alt="TratamentoWeb" class="header-logo-img">
                         <h1>Sistema de Avaliação Psicológica</h1>
                     </div>
                     <div class="user-info">
