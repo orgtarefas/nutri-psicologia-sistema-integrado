8e7cf89adf676a8d9a3d463b6fa91f99b8294c63
orgtarefas <orgtarefas.site@gmail.com>
2026-04-08 23:24:53 -0300

    Update 0_firebase_api_config.js

--- 0_firebase_api_config.js
+++ 0_firebase_api_config.js
@@ -1,21 +1,47 @@
-// Firebase Configuration File
+// Firebase Configuration File - Tudo relacionado ao Firebase está aqui
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    where, 
+    orderBy, 
+    limit,
+    doc,
+    updateDoc,
+    deleteDoc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAO8iQ-Fo16MFjq17MDKvqUFT3n2tlHyFc",
-  authDomain: "data-base-online-evox1.firebaseapp.com",
-  databaseURL: "https://data-base-online-evox1-default-rtdb.firebaseio.com",
-  projectId: "data-base-online-evox1",
-  storageBucket: "data-base-online-evox1.firebasestorage.app",
-  messagingSenderId: "972957988116",
-  appId: "1:972957988116:web:072faae5fdfcceab767e01"
+    apiKey: "AIzaSyAO8iQ-Fo16MFjq17MDKvqUFT3n2tlHyFc",
+    authDomain: "data-base-online-evox1.firebaseapp.com",
+    databaseURL: "https://data-base-online-evox1-default-rtdb.firebaseio.com",
+    projectId: "data-base-online-evox1",
+    storageBucket: "data-base-online-evox1.firebasestorage.app",
+    messagingSenderId: "972957988116",
+    appId: "1:972957988116:web:072faae5fdfcceab767e01"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Export database instance
-export { db };
+// Export tudo que será usado nos outros arquivos
+export { 
+    db, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    where, 
+    orderBy, 
+    limit,
+    doc,
+    updateDoc,
+    deleteDoc,
+    getDoc
+};
